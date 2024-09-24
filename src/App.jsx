1e0269578423cc88c66c211094ee2bb8a4c02085
skonstantis <skonstantis@uth.gr>
2024-09-24 15:43:25 +0300

    fixes and visual improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-// App.jsx
-import React from 'react';
-import NotFound from './components/NotFound';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import Report from './components/Report';
-import NavBar from './components/NavBar'; // Import the NavBar component
-import Footer from './components/Footer'; // Import the Footer component
-import './App.css';
+import React from "react";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Header from "./components/Header";
+import "./App.css";
 
 const App = () => {
-    return (
-        <Router>
-            <div className="app-container">
-                <NavBar /> 
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/report" element={<Report />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-
-                <Footer /> 
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./header.module.css";
+
+const Header = () => {
+  const location = useLocation();
+
+  const isAuthPage =
+    location.pathname !== "/";
+
+  return (
+    <nav className={styles.header}>
+      <div className={styles.navLeft}>
+        <Link to="/">
+          <img className={styles.logo} src="../assets/logo.svg" alt="Αρχική" />
+        </Link>
+        <h1 className={styles.brandname}>Seismologos.gr</h1>
+      </div>
+      <div className={styles.navRight}>
+        <ul>
+          {isAuthPage && (
+            <li>
+              <Link to="/">Αρχική</Link>
+            </li>
+          )}
+          <li>
+            <Link to="/login">Είσοδος</Link>
+          </li>
+          <li>
+            <Link to="/register">Εγγραφή</Link>
+          </li>
+        </ul>
+        <a
+          href="https://x.com/seismologos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={styles.x} src="../assets/x.svg" alt="X" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
