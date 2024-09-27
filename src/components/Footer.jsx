378426b4e24aa3c2963649319a697df609dde5ea
skonstantis <skonstantis@uth.gr>
2024-09-27 18:05:14 +0300

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,31 @@ import styles from "./footer.module.css";
 
 const Footer = () => {
   const getCurrentYear = () => {
-    const options = { timeZone: 'Europe/Athens', year: 'numeric' };
-    return new Intl.DateTimeFormat('en-US', options).format(new Date());
+    const options = { timeZone: "Europe/Athens", year: "numeric" };
+    return new Intl.DateTimeFormat("en-US", options).format(new Date());
   };
 
   return (
     <div className={styles.footer}>
       <div className={styles.linksContainer}>
-        <a href="/privacy-policy" className={styles.link} target="_blank" rel="noopener noreferrer">Απόρρητο</a>
+        <Link to="/privacy-policy" className={styles.link}>
+          Απόρρητο
+        </Link>
         <span>|</span>
-        <a href="/terms-of-service" className={styles.link} target="_blank" rel="noopener noreferrer">Όροι</a>
+
+        <Link to="/terms-of-service" className={styles.link}>
+          Όροι
+        </Link>
         <span>|</span>
-        <a href="/faq" className={styles.link} target="_blank" rel="noopener noreferrer">FAQ</a>
+
+        <Link to="/faq" className={styles.link}>
+          FAQ
+        </Link>
         <span>|</span>
-        <a href="/contact-us" className={styles.link} target="_blank" rel="noopener noreferrer">Επικοινωνία</a>
+
+        <Link to="/contact-us" className={styles.link}>
+          Επικοινωνία
+        </Link>
       </div>
       <span className={styles.copyRight}>&copy; {getCurrentYear()}</span>
     </div>
